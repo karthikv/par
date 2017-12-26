32f2801a9c51561589e67d323762146fdcb1a14e
Karthik Viswanathan <karthik.ksv@gmail.com>
2017-12-25 20:30:25 -0800

    Implement pretty printing.

--- src/par_native.erl
+++ src/par_native.erl
@@ -5,7 +5,9 @@
   gm_set/3,
   concat/2,
   separate/2,
-  to_str/1
+  to_str/1,
+  to_pretty/3,
+  to_iolist/1
 ]).
 
 init(Mod) ->
@@ -81,12 +83,70 @@ separate(Left, Right) ->
 to_str(Str) when is_binary(Str) -> Str;
 to_str(Term) -> iolist_to_binary(to_iolist(Term)).
 
-to_iolist(Int) when is_integer(Int) -> integer_to_binary(Int);
+to_pretty(Str, _, _) when is_binary(Str) -> Str;
+to_pretty(Term, MaxLength, Cur) ->
+  List = wrap(with_length(to_iolist(Term)), MaxLength, Cur),
+  iolist_to_binary(List).
+
+with_length(List) ->
+  lists:mapfoldl(fun
+    (Elem, Accum) when is_list(Elem) ->
+      {SubList, Length} = with_length(Elem),
+      {{SubList, Length}, Accum + Length};
+    (Elem, Accum) when is_integer(Elem) -> {Elem, Accum + 1};
+    % Length just needs to be approximate; use byte_size instead of computing
+    % actual number of unicode characters.
+    (Elem, Accum) when is_binary(Elem) -> {Elem, Accum + byte_size(Elem)}
+  end, 0, List).
+
+wrap({List, Length}, MaxLength, Cur) ->
+  if
+    Cur + Length =< MaxLength -> strip_lengths(List);
+    true ->
+      case List of
+        [$(=Before, {Elems, _}, $)=After] -> ok;
+        [Atom, $(, {Elems, _}, $)=After] -> Before = [Atom, $(];
+        [$[=Before, {Elems, _}, $]=After] -> ok;
+        [$#, $[, {Elems, _}, $]=After] -> Before = [$#, $[];
+        [${=Before, $\s, {Elems, _}, $\s, $}=After] -> ok;
+        [Prefix, ${, $\s, {Elems, _}, $\s, $}=After] -> Before = [Prefix, ${];
+        Before -> Elems = After = []
+      end,
+
+      Padding = [$\n, lists:duplicate(Cur + 2, $\s)],
+      RemoveComma = case Before of
+        B when B == $[; B == ${ -> true;
+        [_, B] when B == $[; B == ${ -> true;
+        _ -> false
+      end,
+
+      {WrappedElems, _} = lists:mapfoldl(fun
+        (Elem, _) when is_tuple(Elem) ->
+          {[Padding, wrap(Elem, MaxLength, Cur + 2)], false};
+        ($,, _) when RemoveComma -> {[], true};
+        ($,, _) -> {$,, true};
+        ($\s, true) -> {[], false};
+        (Elem, _) -> {Elem, false}
+      end, false, Elems),
+      WrappedAfter = case After of
+        [] -> [];
+        _ -> [$\n, lists:duplicate(Cur, $\s), After]
+      end,
+
+      [strip_lengths(Before), WrappedElems, WrappedAfter]
+  end.
+
+strip_lengths({List, _}) -> strip_lengths(List);
+strip_lengths([]) -> [];
+strip_lengths([H | T]) -> [strip_lengths(H) | strip_lengths(T)];
+strip_lengths(E) -> E.
+
+to_iolist(Int) when is_integer(Int) -> [integer_to_binary(Int)];
 % Can't use float_to_binary() for float, since it shows too much precision,
 % so fallback to the last clause.
 % Atom also accounts for booleans. Note that we don't fallback for atoms
 % specifically because formatting a capitalized atom includes single quotes.
-to_iolist(Atom) when is_atom(Atom) -> atom_to_binary(Atom, utf8);
+to_iolist(Atom) when is_atom(Atom) -> [atom_to_binary(Atom, utf8)];
 to_iolist(Str) when is_binary(Str) -> [$", Str, $"];
 to_iolist(Tuple) when is_atom(element(1, Tuple)) ->
   [Atom | Rest] = tuple_to_list(Tuple),
@@ -101,7 +161,7 @@ to_iolist(#{'_@type' := 'Set'}=Map) ->
 to_iolist(#{'_@type' := Type}=Map) ->
   FieldMap = maps:remove('_@type', Map),
   case maps:size(FieldMap) of
-    0 -> <<"{}">>;
+    0 -> [<<"{}">>];
     _ ->
       Fields = lists:map(fun({K, V}) ->
         [to_iolist(K), <<" = ">>, to_iolist(V)]
@@ -112,13 +172,13 @@ to_iolist(#{'_@type' := Type}=Map) ->
       end,
       [Prefix, ${, $\s, comma_sep(Fields), $\s, $}]
   end;
-to_iolist(Map) when map_size(Map) == 0 -> <<"{}">>;
+to_iolist(Map) when map_size(Map) == 0 -> [<<"{}">>];
 to_iolist(Map) when is_map(Map) ->
   Pairs = lists:map(fun({K, V}) ->
     [to_iolist(K), <<" => ">>, to_iolist(V)]
   end, maps:to_list(Map)),
   [${, $\s, comma_sep(Pairs), $\s, $}];
-to_iolist(Term) -> io_lib:format("~p", [Term]).
+to_iolist(Term) -> [iolist_to_binary(io_lib:format("~p", [Term]))].
 
 comma_sep([]) -> [];
 comma_sep([H]) -> [H];
